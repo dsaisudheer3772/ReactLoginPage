88b6291d66f35af28f0c3d016071e181536792d5
Sai Sudheer <doppalapudi94@yahoo.com>
2023-09-27 17:51:46 +0530

    sudheer

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
   <BrowserRouter basename={baseUrl}>
     <App />
   </BrowserRouter>);
-
+// sudheer changes
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
